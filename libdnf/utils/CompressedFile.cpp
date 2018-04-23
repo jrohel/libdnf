f0c3726862365efa43fe8c46658a7d225227f291
Martin Hatina <mhatina@redhat.com>
2018-04-23 09:37:03 +0200

    load & filter module rpms

---
+++ libdnf/utils/CompressedFile.cpp
@@ -0,0 +1,44 @@
+#include "CompressedFile.hpp"
+#include <utility>
+#include <sstream>
+extern "C" {
+#   include <solv/solv_xfopen.h>
+};
+
+libdnf::CompressedFile::CompressedFile(const std::string &filePath)
+        : File(filePath)
+{}
+
+libdnf::CompressedFile::~CompressedFile() = default;
+
+void libdnf::CompressedFile::open(const char *mode)
+{
+    file = solv_xfopen(filePath.c_str(), mode);
+}
+
+void libdnf::CompressedFile::close()
+{
+    fclose(file);
+}
+
+std::string libdnf::CompressedFile::getContent()
+{
+    std::ostringstream ss;
+
+    if (!file) {
+        throw NotOpenedException(filePath);
+    }
+
+    while (true) {
+        constexpr size_t fileSize = 2048;
+        auto content = new char[fileSize + 1];
+        auto bytesRead = read(content, fileSize);
+
+        ss << content;
+        delete [] content;
+
+        if (bytesRead != fileSize) {
+            return ss.str();
+        }
+    }
+}
